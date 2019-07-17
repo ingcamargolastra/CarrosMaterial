c40f75b83edbe67eeb8e09f37056f3d1eeec7c02
Andres Felipe Camargo Lastra <ingcamargolastra@gmail.com>
2019-07-17 10:24:19 -0500

    Interfaz agregar carro e item carro terminado

---
+++ app/src/main/java/com/edu/unimagdalena/carros/AgregarCarro.java
@@ -0,0 +1,14 @@
+package com.edu.unimagdalena.carros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarCarro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_carro);
+    }
+}
